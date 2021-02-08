fd10030c92b59217ad4baf6a0706c7f58fa072ba
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-02-08 09:15:51 +0100

    Add files via upload

---
+++ Windows/Windows/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Notepad")
+            {
+                System.Diagnostics.Process.Start(@"C:\Windows\System32\Notepad.exe");
+            }
+            else
+            {
+                System.Diagnostics.Process.Start(@"C:\Windows\System32\mspaint.exe");
+            }
+        }
+    }
+}
